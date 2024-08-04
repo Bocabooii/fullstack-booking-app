72f14b101b3e0bcf1b5ad7f7fdff9ad75fe3405a
Alexis Bocanegra <alexisbocanegra14@gmail.com>
2024-08-04 17:53:43 -0500

    Login in feature now working. Now fetches data to compare registered users with email and password and if data exists, will allow user to login.

--- api/index.js
+++ api/index.js
@@ -30,15 +30,38 @@ app.get('/test', (req, res) => {
 
 app.post('/register', async (req,res) => {
   const {name, email, password} = req.body;
-  const newUser = await User.create ({
-    name,
-    email,
-    password:bcrypt.hashSync(password, bcryptSalt),
-  })
+  try {
+    const newUser = await User.create ({
+      name,
+      email,
+      password:bcrypt.hashSync(password, bcryptSalt),
+    })
+    res.json(newUser);
+} catch(e) {
+  res.status(422).json(e);
+}
 
-  res.json(newUser);
-})
+});
+
+
+app.post('/login', async (req,res) => {
+  const {email,password} = req.body;
+  const userLogin = await User.findOne({email});
+  if (userLogin) {
+    const passOk = bcrypt.compareSync(password, userLogin.password)
+    if (passOk) {
+      res.json("password ok");
+    } else {
+      res.json("password not ok");
+    }
+  } else {
+    res.status(422).json("not found");
+  }
+});
+
+
 // Start the server and listen for connections on port 4000
 app.listen(4000, () => {
   console.log('Server is listening on port 4000');
 });
+
